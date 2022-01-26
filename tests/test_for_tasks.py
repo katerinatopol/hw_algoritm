8b85e93042fe7aeee8b360529edc7079a8d7a9ff
katerina_topol <katerina_topol@mail.ru>
2022-01-26 10:17:04 +0300

    upd test alphabet

--- tests/test_for_tasks.py
+++ tests/test_for_tasks.py
@@ -1,10 +1,12 @@
 import pytest
 
-from tests.alphabet import alphabet
+from tasks import alphabet
 
 
 @pytest.mark.parametrize('s, true_result', [('MR Robot', '13,18,18,15,2,15,20'),
-                                            ()])
+                                            ('Aa Bb', '1,1,2,2'),
+                                            ('кириллица', 'Некорректная исходная строка'),
+                                            ('!-text', 'Некорректная исходная строка')])
 def test_alphabet(s, true_result):
     check = alphabet(s)
     assert check == true_result, f"Получен результат {check}, ожидался {true_result}"
